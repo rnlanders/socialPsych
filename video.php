de9cde76bcb6fbfaeca82d149e79b7f2aa003026
rnlanders <rnlanders@odu.edu>
2013-10-20 19:43:43 -0400

    Create video.php

---
+++ video.php
@@ -0,0 +1,13 @@
+<?
+require('functionlib.php');
+
+printhead("p { margin: 1em 0 1em 0; }");
+startbody();
+
+?><center>
+<object width="720" height="480"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=11500513&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=1&amp;color=&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=11500513&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=1&amp;color=&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="720" height="480"></embed></object></center>
+<p>If you have questions, you can e-mail the research team at <a href="mailto:tntlab@odu.edu">tntlab@odu.edu</a>.</p>
+<p>Aftering viewing this video, you might want to <a href="http://social.tntlab.org">return to the login page</a> for socialPsych.</p>
+<?
+endbody();
+?>
